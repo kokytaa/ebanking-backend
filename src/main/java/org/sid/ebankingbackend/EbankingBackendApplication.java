539d1770a40246fbd08860925222b4a77ba190e5
kokyta <khalloufkawtar09@gmail.com>
2026-05-06 21:48:57 +0100

    5 Couche service,

--- src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
@@ -1,17 +1,22 @@
 package org.sid.ebankingbackend;
 
+import org.sid.ebankingbackend.Exeption.BalanceNotSufficientException;
+import org.sid.ebankingbackend.Exeption.BankAccountNotFoundException;
+import org.sid.ebankingbackend.Exeption.CustomerNotFoundException;
 import org.sid.ebankingbackend.entities.*;
 import org.sid.ebankingbackend.enums.AccountStatus;
 import org.sid.ebankingbackend.enums.OperationType;
 import org.sid.ebankingbackend.repositories.AccountOperationRepository;
 import org.sid.ebankingbackend.repositories.BankAccountRepository;
 import org.sid.ebankingbackend.repositories.CustomerRepository;
+import org.sid.ebankingbackend.services.BankAccountService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -22,25 +27,32 @@ public class EbankingBackendApplication {
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
 @Bean
-CommandLineRunner commandLineRunner(BankAccountRepository      bankAccountRepository){
-        return args -> { BankAccount bankAccount=
-                bankAccountRepository.findById("045986b1-878b-42fa-a564-a32867587d70").orElse(null);
-            if(bankAccount!=null){
-                System.out.println("***********************************");
-                System.out.println(bankAccount.getId());
-                System.out.println(bankAccount.getBalance());
-                System.out.println(bankAccount.getStatus());
-                System.out.println(bankAccount.getCreatdAt());
-                System.out.println(bankAccount.getCustomer().getName());
-                System.out.println(bankAccount.getClass().getSimpleName());
-                if(bankAccount instanceof CurrentAccount){
-                    System.out.println("Over Draft=>"+((CurrentAccount)bankAccount).getOverDraft());
-                } else if(bankAccount instanceof SavingAccount){
-                    System.out.println("Rate=>"+((SavingAccount)bankAccount).getInterestRate());
-                }
-                bankAccount.getAccountOperations().forEach(op->{
-                    System.out.println(op.getType()+"\t"+op.getOperationDate()+"\t"+op.getAmount());
-                });}};
+CommandLineRunner commandLineRunner(BankAccountService bankAccountService) {
+        return args -> {
+            Stream.of("soso","koky","ahmed").forEach(name->{
+                Customer customer=new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                bankAccountService.saveCustomer(customer);
+            });
+            bankAccountService.listCustomers().forEach(customer->{
+                try {
+                    bankAccountService.saveCurrentBankAccount( Math.random()*90000,  9000, customer.getId());
+                    bankAccountService.saveSavingBankAccount( Math.random()*120000,5.5, customer.getId());
+                    List <BankAccount> bankAccounts=bankAccountService.bankAccounts();
+                    for (BankAccount bankAccount:bankAccounts){
+                        for (int i = 0; i < 10 ; i++) {
+                            bankAccountService.credit(bankAccount.getId(),10000+Math.random()*120000,"Credit");
+                            bankAccountService.debit(bankAccount.getId(),1000+Math.random()*9000, "Debit");
+                        }
+                    }
+                } catch (CustomerNotFoundException e) {
+                    e.printStackTrace();
+                } catch (BankAccountNotFoundException | BalanceNotSufficientException e ) {
+                    throw new RuntimeException(e);}
+
+            });
+        };
 }
 //@Bean
 CommandLineRunner start(CustomerRepository customerRepository,
